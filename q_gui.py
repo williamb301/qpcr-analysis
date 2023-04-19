b73d8d0b9ce6170c8cd9e127af3286b6d8984026
William Boudy <williamboudy@Williams-MacBook-Air.local>
2023-04-19 00:57:35 -0400

    started gui file

---
+++ q_gui.py
@@ -0,0 +1,11 @@
+import tk
+
+def create_gui():
+    root = Tk()
+    root.title("QPCR Analysis")
+
+    mainframe = ttk.Frame(root, padding="3 3 12 12")
+    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    return
